66f73a27f91d1e4c41d7f64ecb43cca0cd78a88f
Hicham0423 <hichammarbouh0423@gmail.com>
2025-07-08 11:09:36 +0100

    Update project with new code

--- src/main.tsx
+++ src/main.tsx
@@ -6,11 +6,7 @@ import { HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-<<<<<<< HEAD
     <HashRouter>
-=======
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
->>>>>>> e78260e2c2c44b7adcbc3c5065dceeeddd466f0e
       <App />
     </HashRouter>
   </React.StrictMode>,
